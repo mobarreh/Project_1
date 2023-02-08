f9c39652dcf7ec04423a6e36b0bb8faeaed87467
shtuupid <mohamedbarreh8@gmail.com>
2023-02-08 20:13:22 +0000

    fixes to javascipt and start of adding additional API

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,23 @@
 //Initial References
 let result = document.getElementById("result");
 let searchBtn = document.getElementById("search-btn");
+let searchBtn2 = document.getElementById("search-btn-two")
 let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let endpointURLSpoonacular = "https://api.spoonacular.com/food/ingredients/substitutes?";
+let appKeySpoon = "1b359d0128f940919c6585bbcb7e80c1";
+
+function searchHistory(q) {
+  console.log(q);
+  // get items from local storage, if there are nothing in local storage set to empty array, need JSON parsing it from a string
+  var searchedIngredients = JSON.parse(localStorage.getItem('ingredients-list')) || [];
+  // pushing new values to the array
+  searchedIngredients.push(q);
+  // reset local history, JSON to put it in back to string in local storage
+  localStorage.setItem('ingredients-list', JSON.stringify(searchedIngredients));
+  renderSearchHistory();
+
+}
+
 
 //Excute MealDB API search once button is clicked
 searchBtn.addEventListener("click", () => {
@@ -31,9 +47,22 @@ searchBtn.addEventListener("click", () => {
         document.getElementById('meal').innerHTML = (myMeal.strMeal);
         document.getElementById('area').innerHTML = (myMeal.strArea);
         document.getElementById('instructions').innerHTML = (myMeal.strInstructions);
+        
+        var queryURLSpoonacular = endpointURLSpoonacular + "apiKey=" + appKeySpoon + "&ingredientName=";
+        fetch(queryURLSpoonacular) // wait for this fetch to finish
+            .then(function (response) {
+                return response.json();
+              
+              })
       })
       .catch(() => {
         result.innerHTML = `<h3>Invalid Input</h3>`;
       });
-  }
-});
+
+
+  };
+})
+ 
+ searchBtn2.addEventListener("click", () =>{
+
+})
